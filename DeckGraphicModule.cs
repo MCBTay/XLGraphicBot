7b3725adc8c242a64cf2d9951f8d9ff59f7480b2
Bryan Taylor <mcbtay@gmail.com>
2021-10-02 11:39:51 -0400

    first pass at implementations for !deck and !shirt commands

--- DeckGraphicModule.cs
+++ DeckGraphicModule.cs
@@ -1,43 +1,173 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
+using System.Drawing;
 
 namespace DeckGraphicBot 
 {
     public class DeckGraphicModule : ModuleBase<SocketCommandContext>
     {
-        [Command("say")]
-        [Summary("Echos a message.")]
-        public Task SayAsync(
-          [Remainder] 
-          [Summary("The text to echo")] 
-          string echo) => ReplyAsync(echo);
-
         [Command("deck")]
         [Summary("Applies the SkaterXL deck template to the most recent image.")]
         public async Task DeckAsync()
         {
-          Console.WriteLine("Running deck");
+          Bitmap attachmentImage = null;
+          Bitmap template = null;
+          Bitmap deck = null;
+          Bitmap wear = null;
 
-          var messages = await Context.Channel.GetMessagesAsync(20).FlattenAsync();
-          if (messages == null || messages.Count() == 0) return;
+          string attachmentFileName = string.Empty;
+          string attachmentFilePath = string.Empty;
 
-          Console.WriteLine("Got some messages!");
+          string deckFileName = string.Empty;
+          string deckFilePath = string.Empty;
 
-          var attachmentMessage = messages.FirstOrDefault(x => x.Attachments.Any());
-          if (attachmentMessage?.Attachments == null) return;
+          try 
+          {
+              (attachmentImage, attachmentFileName) = await GetMostRecentImage();
+              if (attachmentImage == null || string.IsNullOrEmpty(attachmentFileName)) return;
 
-          var attachment = attachmentMessage.Attachments.FirstOrDefault();
-          if (attachmentMessage == null) return;
+              template = new Bitmap("./template.png");
+              if (template == null) return;
 
-          Console.WriteLine("Found an image message!");
+              wear = new Bitmap("./wear2.png");
+              if (wear == null) return;
 
-          await Context.Channel.SendMessageAsync(attachment.Url);
+              deck = new Bitmap(template.Width, template.Height);
 
-          // Context.Channel.SendFileAsync()
+              using (Graphics g = Graphics.FromImage(deck)) 
+              {
+                  g.Clear(System.Drawing.Color.Transparent);
+
+                  g.DrawImage(attachmentImage, new Rectangle(0, 694, template.Width, 482));
+                  g.DrawImage(template, 0, 0, template.Width, template.Height);
+                  g.DrawImage(wear, 0, 0, template.Width, template.Height);
+              }
+
+              deckFilePath = $"./Deck_{attachmentFileName}.png";
+              deck.Save(deckFilePath, System.Drawing.Imaging.ImageFormat.Png);
+
+              await Context.Channel.SendFileAsync(deckFilePath);
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine(ex.Message);
+            throw;
+          }
+          finally 
+          {
+              attachmentImage?.Dispose();
+              template?.Dispose();
+              deck?.Dispose();
+              wear?.Dispose();
+
+              if (!string.IsNullOrEmpty(attachmentFilePath)
+                  && File.Exists(attachmentFilePath)) 
+              {
+                  File.Delete(attachmentFilePath);
+              }
+
+              if (!string.IsNullOrEmpty(deckFilePath)
+                  && File.Exists(deckFilePath))
+              {
+                  File.Delete(deckFilePath);
+              }
+            }
+        }
+    
+        [Command("shirt")]
+        [Summary("Applies the image to the SkaterXL shirt template.")]
+        public async Task ShirtAsync() 
+        {
+          Bitmap attachmentImage = null;
+          Bitmap template = null;
+          Bitmap shirt = null;
+
+          string attachmentFileName = string.Empty;
+          string attachmentFilePath = string.Empty;
+
+          string shirtFileName = string.Empty;
+          string shirtFilePath = string.Empty;
+
+          try 
+          {
+              (attachmentImage, attachmentFileName) = await GetMostRecentImage();
+              if (attachmentImage == null || string.IsNullOrEmpty(attachmentFileName)) return;
+
+              template = new Bitmap("./shirt_template.png");
+              if (template == null) return;
+
+              shirt = new Bitmap(template.Width, template.Height);
+
+              using (Graphics g = Graphics.FromImage(shirt)) 
+              {
+                  g.Clear(System.Drawing.Color.Transparent);
+
+                  g.DrawImage(template, 0, 0, template.Width, template.Height);
+                  g.DrawImage(attachmentImage, new Rectangle(0, 694, template.Width, 482));  
+              }
+
+              shirtFilePath = $"./MShirt_{attachmentFileName}.png";
+              shirt.Save(shirtFilePath, System.Drawing.Imaging.ImageFormat.Png);
+
+              await Context.Channel.SendFileAsync(shirtFilePath);
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine(ex.Message);
+            throw;
+          }
+          finally 
+          {
+              attachmentImage?.Dispose();
+              template?.Dispose();
+              shirt?.Dispose();
+
+              if (!string.IsNullOrEmpty(attachmentFilePath)
+                  && File.Exists(attachmentFilePath)) 
+              {
+                  File.Delete(attachmentFilePath);
+              }
+
+              if (!string.IsNullOrEmpty(shirtFilePath)
+                  && File.Exists(shirtFilePath))
+              {
+                  File.Delete(shirtFilePath);
+              }
+            }
+        }
+
+        private async Task<(Bitmap Image, string fileName)> GetMostRecentImage() 
+        {
+              Bitmap image = null;
+              var fileName = string.Empty;
+
+              var messages = await Context.Channel.GetMessagesAsync(20).FlattenAsync();
+              if (messages == null || messages.Count() == 0) return (image, fileName);
+
+              var attachmentMessage = messages.FirstOrDefault(x => x.Attachments.Any());
+              var attachment = attachmentMessage?.Attachments?.FirstOrDefault();
+              if (attachment == null) return (image, fileName);
+
+              // these will be null according to spec if not an image
+              if (attachment.Height == null || attachment.Width == null) return (image, fileName);
+
+              fileName = attachment.Filename;
+
+              using (var client = new HttpClient())
+              {
+                var bytes = await client.GetByteArrayAsync(attachment.Url);
+                
+                var filePath = $"./{fileName}";
+                File.WriteAllBytes(filePath, bytes);
+                image = new Bitmap(filePath);
+              }
+
+              return (image, fileName);
         }
     }
 }
