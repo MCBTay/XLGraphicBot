624bc8324912d2efddf9c17887e36981b00e2524
Bryan Taylor <mcbtay@gmail.com>
2021-10-02 17:46:10 -0400

    whitespace cleanup tab spacing

--- modules/DeckGraphicModule.cs
+++ modules/DeckGraphicModule.cs
@@ -11,58 +11,58 @@ namespace XLGraphicBot
         [Summary("Applies the SkaterXL deck template to the most recent image.")]
         public async Task DeckAsync()
         {
-          Bitmap attachmentImage = null;
-          Bitmap template = null;
-          Bitmap deck = null;
-          Bitmap wear = null;
+            Bitmap attachmentImage = null;
+            Bitmap template = null;
+            Bitmap deck = null;
+            Bitmap wear = null;
 
-          string attachmentFileName = string.Empty;
-          string attachmentFilePath = string.Empty;
+            string attachmentFileName = string.Empty;
+            string attachmentFilePath = string.Empty;
 
-          string deckFileName = string.Empty;
-          string deckFilePath = string.Empty;
+            string deckFileName = string.Empty;
+            string deckFilePath = string.Empty;
 
-          try 
-          {
-              (attachmentImage, attachmentFileName) = await Utilities.GetMostRecentImage(Context.Channel);
-              if (attachmentImage == null || string.IsNullOrEmpty(attachmentFileName)) return;
+            try 
+            {
+                (attachmentImage, attachmentFileName) = await Utilities.GetMostRecentImage(Context.Channel);
+                if (attachmentImage == null || string.IsNullOrEmpty(attachmentFileName)) return;
 
-              template = new Bitmap("./img/templates/deck.png");
-              if (template == null) return;
+                template = new Bitmap("./img/templates/deck.png");
+                if (template == null) return;
 
-              wear = new Bitmap("./img/templates/wear.png");
-              if (wear == null) return;
+                wear = new Bitmap("./img/templates/wear.png");
+                if (wear == null) return;
 
-              deck = new Bitmap(template.Width, template.Height);
+                deck = new Bitmap(template.Width, template.Height);
 
-              using (Graphics g = Graphics.FromImage(deck)) 
-              {
-                  g.Clear(System.Drawing.Color.Transparent);
+                using (Graphics g = Graphics.FromImage(deck)) 
+                {
+                    g.Clear(System.Drawing.Color.Transparent);
 
-                  g.DrawImage(attachmentImage, new Rectangle(0, 694, template.Width, 482));
-                  g.DrawImage(template, 0, 0, template.Width, template.Height);
-                  g.DrawImage(wear, 0, 0, template.Width, template.Height);
-              }
+                    g.DrawImage(attachmentImage, new Rectangle(0, 694, template.Width, 482));
+                    g.DrawImage(template, 0, 0, template.Width, template.Height);
+                    g.DrawImage(wear, 0, 0, template.Width, template.Height);
+                }
 
-              deckFilePath = $"./Deck_{attachmentFileName}.png";
-              deck.Save(deckFilePath, System.Drawing.Imaging.ImageFormat.Png);
+                deckFilePath = $"./Deck_{attachmentFileName}.png";
+                deck.Save(deckFilePath, System.Drawing.Imaging.ImageFormat.Png);
 
-              await Context.Channel.SendFileAsync(deckFilePath);
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine(ex.Message);
-            throw;
-          }
-          finally 
-          {
-              attachmentImage?.Dispose();
-              template?.Dispose();
-              deck?.Dispose();
-              wear?.Dispose();
+                await Context.Channel.SendFileAsync(deckFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally 
+            {
+                attachmentImage?.Dispose();
+                template?.Dispose();
+                deck?.Dispose();
+                wear?.Dispose();
 
-              Utilities.DeleteFile(attachmentFilePath);
-              Utilities.DeleteFile(deckFilePath);
+                Utilities.DeleteFile(attachmentFilePath);
+                Utilities.DeleteFile(deckFilePath);
             }
         }
     }
