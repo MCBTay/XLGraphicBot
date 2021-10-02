347091bc9bec0b3cbaacfc350e979d58a4f316e6
Bryan Taylor <mcbtay@gmail.com>
2021-10-01 23:39:12 -0400

    first implementation of passing in the application otken and logging in to discord with the bot

--- DeckGraphicBot.cs
+++ DeckGraphicBot.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DeckGraphicBot
 {
     public class DeckGraphicBot
     {
-        static void Main(string[] args)
+        private const string APP_TOKEN = "app_token_here";
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new DeckGraphicBot().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync() 
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, APP_TOKEN);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg) 
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
