fee9537577d009b1cd55cb85d42d5220032a08db
Bryan Taylor <mcbtay@gmail.com>
2021-11-02 22:00:19 -0400

    adding a localization project and moving all of the help strings into it

--- modules/HelpModule.cs
+++ modules/HelpModule.cs
@@ -2,10 +2,11 @@ using System;
 using System.Threading.Tasks;
 using Discord.Commands;
 using System.Text;
+using XLGraphicBot.Localization;
 
 namespace XLGraphicBot
 {
-  [Group("graphicbot")]
+	[Group("graphicbot")]
     public class HelpModule : ModuleBase<SocketCommandContext>
     {
         [Group("help")]
@@ -17,11 +18,11 @@ namespace XLGraphicBot
             {
                 var sb = new StringBuilder();
 
-                sb.Append("**XLGraphicBot Help**" + Environment.NewLine);
-                sb.Append("XLGraphicBot allows you to take images posted in a channel and turn them into a deck or shirt immediately for SkaterXL." + Environment.NewLine);
-                sb.Append("- `!deck` - Takes the most recent image and places it on a deck template.  For more info, run `!graphicbot help deck`." + Environment.NewLine);
-                sb.Append("- `!alphamshirt`/`!bwhoodie`/`!esshirt`/`!mhoodie`/`!mshirt`/`!msweater`/`!tashirt`/`!tlsweater` - Takes the most recent image and places it on the appropriate shirt/sweater template.  For more info, run `!graphicbot help tops`" + Environment.NewLine);
-                sb.Append("  - To see a list of all the colors that can be used with these run `!graphicbot help tops colors`.");
+                sb.AppendLine(Strings.HelpHeader);
+                sb.AppendLine(Strings.General_Description);
+                sb.AppendLine(Strings.General_Deck);
+                sb.AppendLine(Strings.General_Tops);
+                sb.AppendLine(Strings.General_Colors);
 
                 await ReplyAsync(sb.ToString());
             }
@@ -31,11 +32,11 @@ namespace XLGraphicBot
             {
                 var sb = new StringBuilder();
 
-                sb.Append("**XLGraphicBot Help**" + Environment.NewLine);
-                sb.Append("- `!deck` - Takes the most recent image not posted by XLGraphicBot and places it on a deck template." + Environment.NewLine);
-                sb.Append("  - Has two optional parameters, which can be omitted entirely or used in any combination or order:" + Environment.NewLine);
-                sb.Append("    - `includeWear` - Defaults to `false`.  Overlays a board wear pattern on the texture.  Example usage: `!deck includeWear: true`" + Environment.NewLine);
-                sb.Append("    - `maintainAspectRatio` - Defaults to `true`.  Maintains the original images aspect ratio when scaling down.  Example usage: `!deck maintainAspectRatio: false`" + Environment.NewLine);
+                sb.AppendLine(Strings.HelpHeader);
+                sb.AppendLine(Strings.Deck_Description);
+                sb.AppendLine(Strings.Deck_Parameters);
+                sb.AppendLine(Strings.Deck_IncludeWear);
+                sb.AppendLine(Strings.Deck_MaintainAspectRatio);
 
                 await ReplyAsync(sb.ToString());
             }
@@ -43,45 +44,39 @@ namespace XLGraphicBot
             [Group("tops")]
             public class TopsHelpModule : ModuleBase<SocketCommandContext>
             {
+	            [Command]
+				public async Task TopsHelpAsync()
+				{
+					var sb = new StringBuilder();
 
-              [Command]
-              public async Task TopsHelpAsync()
-              {
-                  var sb = new StringBuilder();
+					sb.AppendLine(Strings.HelpHeader);
+					sb.AppendLine(Strings.Tops_Description);
+					sb.AppendLine(Strings.Tops_AlphaMShirt);
+					sb.AppendLine(Strings.Tops_BWHoodie);
+					sb.AppendLine(Strings.Tops_ESShirt);
+					sb.AppendLine(Strings.Tops_MHoodie);
+					sb.AppendLine(Strings.Tops_MShirt);
+					sb.AppendLine(Strings.Tops_MSweater);
+					sb.AppendLine(Strings.Tops_TAShirt);
+					sb.AppendLine(Strings.Tops_TLSweater);
+					sb.AppendLine(Strings.Tops_Parameters);
+					sb.AppendLine(Strings.Tops_Color);
+					sb.AppendLine(Strings.Tops_KnownColors);
+					sb.AppendLine(Strings.Tops_AARRGGBB);
+					sb.AppendLine(Strings.Tops_RRGGBB);
+					sb.AppendLine(Strings.Tops_MaintainAspectRatio);
 
-                  sb.Append("**XLGraphicBot Help**" + Environment.NewLine);
-                  sb.Append("XLGraphicBot supports multiple top types.  They all take the most recent image that was not posted by XLGraphicBot and place it on the appropriate template.  Most have aliases and shortcuts." + Environment.NewLine);
-                  sb.Append("- `!alphamshirt`/`!alpha` - Alpha MShirt template." + Environment.NewLine);
-                  sb.Append("- `!bwhoodie`/`!bw` - BWHoodie template." + Environment.NewLine);
-                  sb.Append("- `!esshirt`/`!es` - ESShirt template." + Environment.NewLine);
-                  sb.Append("- `!mhoodie`/`!hoodie` - MHoodie template." + Environment.NewLine);
-                  sb.Append("- `!mshirt`/`!shirt` - MShirt template." + Environment.NewLine);
-                  sb.Append("- `!msweater`/`!sweater` - MSweater template." + Environment.NewLine);
-                  sb.Append("- `!tashirt`/`!ta` - TAShirt template." + Environment.NewLine);
-                  sb.Append("- `!tlsweater`/`!tl` - TLSweater template." + Environment.NewLine);
-                  sb.Append("  - Each of these commands have two optional parameters which can be omitted entirely or used in any combination or order:" + Environment.NewLine);
-                  sb.Append("    - `color` - Defaults to `null`.  If not passed, the default white of the template will be used.  Can be passed in 3 formats:" + Environment.NewLine);
-                  sb.Append("      - `Known colors` - English monikers for colors.  To see the full list run `!graphicbot help tops colors`." + Environment.NewLine);
-                  sb.Append("      - `#AARRGGBB` - Example usage: `!esshirt color: #FFFF00FF`" + Environment.NewLine);
-                  sb.Append("      - `#RRGGBB` - Example usage: `!tlsweater color: #FF00FF`" + Environment.NewLine);
-                  sb.Append("    - `maintainAspectRatio` - Defaults to `true`.  Maintains the original images aspect ratio when scaling down.  Example usage: `!mshirt maintainAspectRatio: false`" + Environment.NewLine);
-
-                  await ReplyAsync(sb.ToString());
-                }
+					await ReplyAsync(sb.ToString());
+				}
 
                 [Command("colors")]
                 public async Task ColorsHelpAsync()
                 {
                     var sb = new StringBuilder();
 
-                    sb.Append("The full list of colors that can be used with any of the tops commands:" + Environment.NewLine);
-                    sb.Append("AliceBlue, AntiqueWhite, Aqua, Aquamarine, Azure, Beige, Bisque, Black, BlanchedAlmond, Blue, BlueViolet, Brown, BurlyWood, CadetBlue, Chartreuse, Chocolate, Coral, CornflowerBlue, Cornsilk, Crimson, Cyan, ");
-                    sb.Append("DarkBlue, DarkCyan, DarkGoldenrod, DarkGray, DarkGreen, DarkKhaki, DarkMagenta, DarkOliveGreen, DarkOrange, DarkOrchid, DarkRed, DarkSalmon, DarkSeaGreen, DarkSlateBlue, DarkSlateGray, DarkTurquoise, DarkViolet, DeepPink, ");
-                    sb.Append("DeepSkyBlue, DimGray, DodgerBlue, Firebrick, FloralWhite, ForestGreen, Fuchsia, Gainsboro, GhostWhite, Gold, Goldenrod, Gray, Green, GreenYellow, Honeydew, HotPink, IndianRed, Indigo, Ivory, Khaki, Lavender, LavenderBlush, ");
-                    sb.Append("LawnGreen, LemonChiffon, LightBlue, LightCoral, LightCyan, LightGoldenrodYellow, LightGray, LightGreen, LightPink, LightSalmon, LightSeaGreen, LightSkyBlue, LightSlateGray, LightSteelBlue, LightYellow, Lime, LimeGreen, Linen, ");
-                    sb.Append("Magenta, Maroon, MediumAquamarine, MediumBlue, MediumOrchid, MediumPurple, MediumSeaGreen, MediumSlateBlue, MediumSpringGreen, MediumTurquoise, MediumVioletRed, MidnightBlue, MintCream, MistyRose, Moccasin, NavajoWhite, Navy, ");
-                    sb.Append("OldLace, Olive, OliveDrab, Orange, OrangeRed, Orchid, PaleGoldenrod, PaleGreen, PaleTurquoise, PaleVioletRed, PapayaWhip, PeachPuff, Peru, Pink, Plum, PowderBlue, Purple, Red, RosyBrown, RoyalBlue, SaddleBrown, Salmon, SandyBrown, ");
-                    sb.Append("SeaGreen, SeaShell, Sienna, Silver, SkyBlue, SlateBlue, SlateGray, Snow, SpringGreen, SteelBlue, Tan, Teal, Thistle, Tomato, Transparent, Turquoise, Violet, Wheat, White, WhiteSmoke, Yellow, and YellowGreen" + Environment.NewLine);
+                    sb.AppendLine(Strings.HelpHeader);
+                    sb.AppendLine(Strings.TheFullListOfColors);
+                    sb.AppendLine(Strings.KnownColors);
 
                     await ReplyAsync(sb.ToString());
                 }
