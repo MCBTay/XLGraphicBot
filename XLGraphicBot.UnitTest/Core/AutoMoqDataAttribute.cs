37e02e11893019a630e3579856c2ae1c50a7e9c3
Bryan Taylor <mcbtay@gmail.com>
2021-11-02 21:33:12 -0400

    adding automoqdataattribute

---
+++ XLGraphicBot.UnitTest/Core/AutoMoqDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace XLGraphicBot.UnitTest.Core
+{
+	public class AutoMoqDataAttribute : AutoDataAttribute
+	{
+		public AutoMoqDataAttribute() : base(() => { return new Fixture().Customize(new AutoMoqCustomization()); })
+		{
+
+		}
+	}
+}
