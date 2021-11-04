e63e38154ce29a19b907b1e71b9e74e666e6ba5a
Bryan Taylor <mcbtay@gmail.com>
2021-11-03 22:47:28 -0400

    adding tests for DeleteFile and ScaleImage

--- XLGraphicBot.UnitTest/modules/BaseGraphicModuleUnitTest.cs
+++ XLGraphicBot.UnitTest/modules/BaseGraphicModuleUnitTest.cs
@@ -19,6 +19,7 @@ namespace XLGraphicBot.UnitTest.modules
 {
 	public class BaseGraphicModuleUnitTest
 	{
+		#region GetMostRecentImage tests
 		[Theory, AutoMoqData]
 		public async Task GetMostRecentImage_NoMessages(
 			Mock<IMessageChannel> mockMessageChannel,
@@ -259,6 +260,83 @@ namespace XLGraphicBot.UnitTest.modules
 					It.IsAny<byte[]>(),
 					It.IsAny<CancellationToken>()));
 		}
+		#endregion
+
+		#region DeleteFile tests
+		[Theory]
+		[InlineAutoMoqData(null)]
+		[InlineAutoMoqData("")]
+		public async Task DeleteFile_InvalidFilePath(
+			string filepath,
+			[Frozen] Mock<IFileSystem> mockFileSystem,
+			BaseGraphicModule sut)
+		{
+			sut.DeleteFile(filepath);
+
+			mockFileSystem.Verify(x => x.File.Delete(filepath), Times.Never());
+		}
+
+		[Theory, AutoMoqData]
+		public async Task DeleteFile_FileDoesNotExist(
+			string filepath,
+			[Frozen] Mock<IFileSystem> mockFileSystem,
+			BaseGraphicModule sut)
+		{
+			mockFileSystem.Setup(x => x.File.Exists(filepath)).Returns(false);
+
+			sut.DeleteFile(filepath);
+
+			mockFileSystem.Verify(x => x.File.Delete(filepath), Times.Never());
+		}
+
+		[Theory, AutoMoqData]
+		public async Task DeleteFile_FileExists(
+			string filepath,
+			[Frozen] Mock<IFileSystem> mockFileSystem,
+			BaseGraphicModule sut)
+		{
+			mockFileSystem.Setup(x => x.File.Exists(filepath)).Returns(true);
+
+			sut.DeleteFile(filepath);
+
+			mockFileSystem.Verify(x => x.File.Delete(filepath));
+		}
+		#endregion
+
+		#region ScaleImage tests
+		[Theory, AutoMoqData]
+		public void ScaleImage_WidthAndHeightMatch(
+			BaseGraphicModule sut)
+		{
+			var rectangle = new Rectangle(0, 0, 500, 500);
+
+			var actual = sut.ScaleImage(100, 100, rectangle);
+
+			actual.Should().Be(rectangle);
+		}
+
+		[Theory, AutoMoqData]
+		public void ScaleImage_WidthLarger(
+			BaseGraphicModule sut)
+		{
+			var rectangle = new Rectangle(0, 0, 500, 500);
+
+			var actual = sut.ScaleImage(200, 100, rectangle);
+
+			actual.Should().Be(new Rectangle(0, 125, 500, 250));
+		}
+
+		[Theory, AutoMoqData]
+		public void ScaleImage_HeightLarger(
+			BaseGraphicModule sut)
+		{
+			var rectangle = new Rectangle(0, 0, 500, 500);
+
+			var actual = sut.ScaleImage(100, 200, rectangle);
+
+			actual.Should().Be(new Rectangle(125, 0, 250, 500));
+		}
+		#endregion
 
 		public class HttpMessageHandlerStub : HttpMessageHandler
 		{
