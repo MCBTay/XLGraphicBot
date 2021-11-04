e4f1418583be43d9980a1ffc21467fdd8546d3a2
Bryan Taylor <mcbtay@gmail.com>
2021-11-03 22:01:26 -0400

    first pass at unit tests for basegraphicmodule

---
+++ XLGraphicBot.UnitTest/modules/BaseGraphicModuleUnitTest.cs
@@ -0,0 +1,276 @@
+﻿using AutoFixture.Xunit2;
+using Discord;
+using Discord.Commands;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using XLGraphicBot.modules;
+using XLGraphicBot.UnitTest.Core;
+using Xunit;
+
+namespace XLGraphicBot.UnitTest.modules
+{
+	public class BaseGraphicModuleUnitTest
+	{
+		[Theory, AutoMoqData]
+		public async Task GetMostRecentImage_NoMessages(
+			Mock<IMessageChannel> mockMessageChannel,
+			[Frozen] Mock<ICommandContext> mockCommandContext,
+			BaseGraphicModule sut)
+		{
+			mockMessageChannel
+				.Setup(x => x.GetMessagesAsync(
+					It.IsAny<int>(),
+					It.IsAny<CacheMode>(),
+					It.IsAny<RequestOptions>()))
+				.Returns(new List<List<IMessage>>().ToAsyncEnumerable());
+
+			mockCommandContext
+				.SetupGet(x => x.Channel)
+				.Returns(mockMessageChannel.Object);
+
+			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
+
+			image.Should().BeNull();
+			filename.Should().BeEmpty();
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetMostRecentImage_MessagesMatchCurrentUser(
+			List<List<IMessage>> messages,
+			Mock<IDiscordClient> mockDiscordClient,
+			Mock<ISelfUser> mockSelfUser,
+			ulong selfUserId,
+			Mock<IMessageChannel> mockMessageChannel,
+			[Frozen] Mock<ICommandContext> mockCommandContext,
+			BaseGraphicModule sut)
+		{
+			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
+
+			foreach (var messageList in messages)
+			{
+				foreach (var message in messageList)
+				{
+					Mock.Get(message.Author)
+						.SetupGet(x => x.Id)
+						.Returns(mockSelfUser.Object.Id);
+				}
+			}
+
+			mockDiscordClient
+				.SetupGet(x => x.CurrentUser)
+				.Returns(mockSelfUser.Object);
+
+			mockCommandContext
+				.SetupGet(x => x.Client).Returns(mockDiscordClient.Object);
+
+			mockMessageChannel
+				.Setup(x => x.GetMessagesAsync(
+					It.IsAny<int>(),
+					It.IsAny<CacheMode>(),
+					It.IsAny<RequestOptions>()))
+				.Returns(messages.ToAsyncEnumerable());
+
+			mockCommandContext
+				.SetupGet(x => x.Channel)
+				.Returns(mockMessageChannel.Object);
+
+			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
+
+			image.Should().BeNull();
+			filename.Should().BeEmpty();
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetMostRecentImage_MessagesDoNotMatchCurrentUser_NoAttachments(
+			List<List<IMessage>> messages,
+			Mock<IDiscordClient> mockDiscordClient,
+			Mock<ISelfUser> mockSelfUser,
+			ulong selfUserId,
+			ulong otherUserId,
+			Mock<IMessageChannel> mockMessageChannel,
+			[Frozen] Mock<ICommandContext> mockCommandContext,
+			BaseGraphicModule sut)
+		{
+			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
+
+			foreach (var messageList in messages)
+			{
+				foreach (var message in messageList)
+				{
+					Mock.Get(message.Author)
+						.SetupGet(x => x.Id)
+						.Returns(otherUserId);
+				}
+			}
+
+			mockDiscordClient
+				.SetupGet(x => x.CurrentUser)
+				.Returns(mockSelfUser.Object);
+
+			mockCommandContext
+				.SetupGet(x => x.Client).Returns(mockDiscordClient.Object);
+
+			mockMessageChannel
+				.Setup(x => x.GetMessagesAsync(
+					It.IsAny<int>(),
+					It.IsAny<CacheMode>(),
+					It.IsAny<RequestOptions>()))
+				.Returns(messages.ToAsyncEnumerable());
+
+			mockCommandContext
+				.SetupGet(x => x.Channel)
+				.Returns(mockMessageChannel.Object);
+
+			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
+
+			image.Should().BeNull();
+			filename.Should().BeEmpty();
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetMostRecentImage_FoundAttachment_NullHeightAndWidth(
+			List<List<IMessage>> messages,
+			Mock<IDiscordClient> mockDiscordClient,
+			Mock<ISelfUser> mockSelfUser,
+			ulong selfUserId,
+			ulong otherUserId,
+			IReadOnlyList<IAttachment> attachments,
+			Mock<IMessageChannel> mockMessageChannel,
+			[Frozen] Mock<ICommandContext> mockCommandContext,
+			BaseGraphicModule sut)
+		{
+			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
+
+			foreach (var messageList in messages)
+			{
+				foreach (var message in messageList)
+				{
+					Mock.Get(message.Author)
+						.SetupGet(x => x.Id)
+						.Returns(otherUserId);
+
+					Mock.Get(message)
+						.SetupGet(x => x.Attachments)
+						.Returns(attachments);
+				}
+			}
+
+			mockDiscordClient
+				.SetupGet(x => x.CurrentUser)
+				.Returns(mockSelfUser.Object);
+
+			mockCommandContext
+				.SetupGet(x => x.Client).Returns(mockDiscordClient.Object);
+
+			mockMessageChannel
+				.Setup(x => x.GetMessagesAsync(
+					It.IsAny<int>(),
+					It.IsAny<CacheMode>(),
+					It.IsAny<RequestOptions>()))
+				.Returns(messages.ToAsyncEnumerable());
+
+			mockCommandContext
+				.SetupGet(x => x.Channel)
+				.Returns(mockMessageChannel.Object);
+
+			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
+
+			image.Should().BeNull();
+			filename.Should().BeEmpty();
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetMostRecentImage_FoundAttachment_WrittenSuccesfully(
+			List<List<IMessage>> messages,
+			Mock<IDiscordClient> mockDiscordClient,
+			Mock<ISelfUser> mockSelfUser,
+			ulong selfUserId,
+			ulong otherUserId,
+			IReadOnlyList<IAttachment> attachments,
+			Mock<IMessageChannel> mockMessageChannel,
+			[Frozen] Mock<HttpClient> mockHttpClient,
+			[Frozen] Mock<IFileSystem> mockFileSystem,
+			[Frozen] Mock<ICommandContext> mockCommandContext,
+			BaseGraphicModule sut)
+		{
+			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
+
+			foreach (var messageList in messages)
+			{
+				foreach (var message in messageList)
+				{
+					Mock.Get(message.Author)
+						.SetupGet(x => x.Id)
+						.Returns(otherUserId);
+
+					Mock.Get(message)
+						.SetupGet(x => x.Attachments)
+						.Returns(attachments);
+				}
+			}
+
+			foreach (var attachment in attachments)
+			{
+				Mock.Get(attachment).SetupGet(x => x.Height).Returns(100);
+				Mock.Get(attachment).SetupGet(x => x.Width).Returns(100);
+				Mock.Get(attachment).SetupGet(x => x.Url).Returns("www.google.com");
+			}
+
+			mockDiscordClient
+				.SetupGet(x => x.CurrentUser)
+				.Returns(mockSelfUser.Object);
+
+			mockCommandContext
+				.SetupGet(x => x.Client).Returns(mockDiscordClient.Object);
+
+			mockMessageChannel
+				.Setup(x => x.GetMessagesAsync(
+					It.IsAny<int>(),
+					It.IsAny<CacheMode>(),
+					It.IsAny<RequestOptions>()))
+				.Returns(messages.ToAsyncEnumerable());
+
+			mockCommandContext
+				.SetupGet(x => x.Channel)
+				.Returns(mockMessageChannel.Object);
+
+			var test = new HttpClient(new HttpMessageHandlerStub());
+
+			//mockHttpClient
+			//	.Setup(x => x.GetByteArrayAsync(It.IsAny<string>()))
+			//	.ReturnsAsync(new byte[1]);
+
+			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
+
+			image.Should().BeNull();
+			filename.Should().BeEmpty();
+
+			mockFileSystem
+				.Verify(x => x.File.WriteAllBytesAsync(
+					It.IsAny<string>(),
+					It.IsAny<byte[]>(),
+					It.IsAny<CancellationToken>()));
+		}
+
+		public class HttpMessageHandlerStub : HttpMessageHandler
+		{
+			protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+			{
+				var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+				{
+					Content = new StringContent("This is a reply")
+				};
+
+				return await Task.FromResult(responseMessage);
+			}
+		}
+	}
+}
