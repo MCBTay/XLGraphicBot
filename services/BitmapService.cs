8bd6182c0706bd557de5e5731c00e53917874ed8
Bryan Taylor <mcbtay@gmail.com>
2021-11-04 21:34:56 -0400

    creating BitmapService

---
+++ services/BitmapService.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace XLGraphicBot.services
+{
+	public interface IBitmapService
+	{
+		Bitmap CreateBitmap(string filename);
+	}
+
+	public class BitmapService : IBitmapService
+	{
+		public Bitmap CreateBitmap(string filename)
+		{
+			return new Bitmap(filename);
+		}
+	}
+}
