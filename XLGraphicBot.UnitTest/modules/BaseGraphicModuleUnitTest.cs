70f37b88f8ae5363a185c9f6d054b4dd388ab276
Bryan Taylor <mcbtay@gmail.com>
2021-11-05 17:29:40 -0400

    moving unit tests from BaseGraphicModuleUnitTest to DiscordServiceUnitTest

--- XLGraphicBot.UnitTest/modules/BaseGraphicModuleUnitTest.cs
+++ XLGraphicBot.UnitTest/modules/BaseGraphicModuleUnitTest.cs
@@ -1,16 +1,10 @@
 ﻿using AutoFixture.Xunit2;
-using Discord;
-using Discord.Commands;
 using FluentAssertions;
 using Moq;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Threading.Tasks;
 using XLGraphicBot.modules;
-using XLGraphicBot.services;
 using XLGraphicBot.UnitTest.Core;
 using Xunit;
 
@@ -19,150 +13,6 @@ namespace XLGraphicBot.UnitTest.modules
 	[ExcludeFromCodeCoverage]
 	public class BaseGraphicModuleUnitTest
 	{
-		#region GetMostRecentImage tests
-		[Theory, AutoMoqData]
-		public async Task GetMostRecentImage_NoMessages(
-			Mock<ISelfUser> mockSelfUser,
-			[Frozen] Mock<ICommandContext> mockCommandContext,
-			BaseGraphicModule sut)
-		{
-			mockCommandContext.SetupCommandContext(mockSelfUser.Object);
-
-			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
-
-			image.Should().BeNull();
-			filename.Should().BeEmpty();
-		}
-
-		[Theory, AutoMoqData]
-		public async Task GetMostRecentImage_MessagesMatchCurrentUser(
-			List<List<IMessage>> messages,
-			Mock<ISelfUser> mockSelfUser,
-			ulong selfUserId,
-			[Frozen] Mock<ICommandContext> mockCommandContext,
-			BaseGraphicModule sut)
-		{
-			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
-
-			messages.SetupMessages(mockSelfUser.Object.Id);
-
-			mockCommandContext.SetupCommandContext(mockSelfUser.Object, messages);
-
-			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
-
-			image.Should().BeNull();
-			filename.Should().BeEmpty();
-		}
-
-		[Theory, AutoMoqData]
-		public async Task GetMostRecentImage_MessagesDoNotMatchCurrentUser_NoAttachments(
-			List<List<IMessage>> messages,
-			Mock<IDiscordClient> mockDiscordClient,
-			Mock<ISelfUser> mockSelfUser,
-			ulong selfUserId,
-			ulong otherUserId,
-			Mock<IMessageChannel> mockMessageChannel,
-			[Frozen] Mock<ICommandContext> mockCommandContext,
-			BaseGraphicModule sut)
-		{
-			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
-
-			messages.SetupMessages(otherUserId);
-
-			mockDiscordClient
-				.SetupGet(x => x.CurrentUser)
-				.Returns(mockSelfUser.Object);
-
-			mockCommandContext
-				.SetupGet(x => x.Client).Returns(mockDiscordClient.Object);
-
-			mockMessageChannel
-				.Setup(x => x.GetMessagesAsync(
-					It.IsAny<int>(),
-					It.IsAny<CacheMode>(),
-					It.IsAny<RequestOptions>()))
-				.Returns(messages.ToAsyncEnumerable());
-
-			mockCommandContext
-				.SetupGet(x => x.Channel)
-				.Returns(mockMessageChannel.Object);
-
-			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
-
-			image.Should().BeNull();
-			filename.Should().BeEmpty();
-		}
-
-		[Theory, AutoMoqData]
-		public async Task GetMostRecentImage_FoundAttachment_NullHeightAndWidth(
-			List<List<IMessage>> messages,
-			Mock<IDiscordClient> mockDiscordClient,
-			Mock<ISelfUser> mockSelfUser,
-			ulong selfUserId,
-			ulong otherUserId,
-			List<IAttachment> attachments,
-			Mock<IMessageChannel> mockMessageChannel,
-			[Frozen] Mock<ICommandContext> mockCommandContext,
-			BaseGraphicModule sut)
-		{
-			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
-
-			messages.SetupMessages(otherUserId, attachments);
-
-			mockDiscordClient
-				.SetupGet(x => x.CurrentUser)
-				.Returns(mockSelfUser.Object);
-
-			mockCommandContext
-				.SetupGet(x => x.Client)
-				.Returns(mockDiscordClient.Object);
-
-			mockMessageChannel
-				.Setup(x => x.GetMessagesAsync(
-					It.IsAny<int>(),
-					It.IsAny<CacheMode>(),
-					It.IsAny<RequestOptions>()))
-				.Returns(messages.ToAsyncEnumerable());
-
-			mockCommandContext
-				.SetupGet(x => x.Channel)
-				.Returns(mockMessageChannel.Object);
-
-			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
-
-			image.Should().BeNull();
-			filename.Should().BeEmpty();
-		}
-
-		[Theory, AutoMoqData]
-		public async Task GetMostRecentImage_FoundAttachment_WrittenSuccessfully(
-			List<List<IMessage>> messages,
-			Mock<ISelfUser> mockSelfUser,
-			ulong selfUserId,
-			ulong otherUserId,
-			List<IAttachment> attachments,
-			[Frozen] Mock<IBitmapService> mockBitmapService,
-			[Frozen] Mock<ICommandContext> mockCommandContext,
-			BaseGraphicModule sut)
-		{
-			mockSelfUser.SetupGet(x => x.Id).Returns(selfUserId);
-
-			attachments.SetupAttachments();
-			messages.SetupMessages(otherUserId, attachments);
-
-			mockCommandContext.SetupCommandContext(mockSelfUser.Object, messages);
-
-			mockBitmapService
-				.Setup(x => x.CreateBitmap(It.IsAny<string>(), It.IsAny<string>()))
-				.ReturnsAsync(new Bitmap(100, 100));
-
-			(Bitmap image, string filename) = await sut.GetMostRecentImage(mockCommandContext.Object);
-
-			image.Should().NotBeNull();
-			filename.Should().NotBeNullOrEmpty();
-		}
-		#endregion
-
 		#region DeleteFile tests
 		[Theory]
 		[InlineAutoMoqData(null)]
