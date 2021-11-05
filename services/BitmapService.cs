93292f38c21cbc489b332c93acf93bb82ef1e666
Bryan Taylor <mcbtay@gmail.com>
2021-11-04 22:24:41 -0400

    implementing BitmapService

--- services/BitmapService.cs
+++ services/BitmapService.cs
@@ -1,17 +1,45 @@
 ﻿using System.Drawing;
+using System.IO.Abstractions;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace XLGraphicBot.services
 {
 	public interface IBitmapService
 	{
-		Bitmap CreateBitmap(string filename);
+		Task<Bitmap> CreateBitmap(string url, string filename);
 	}
 
 	public class BitmapService : IBitmapService
 	{
-		public Bitmap CreateBitmap(string filename)
+		private readonly IFileSystem _fileSystem;
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public BitmapService(
+			IFileSystem fileSystem, 
+			IHttpClientFactory httpClientFactory)
 		{
-			return new Bitmap(filename);
+			_fileSystem = fileSystem;
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<Bitmap> CreateBitmap(string url, string filename)
+		{
+			Bitmap bitmap = null;
+
+			using (var client = _httpClientFactory.CreateClient())
+			{
+				var bytes = await client.GetByteArrayAsync(url);
+
+				var directory = "./img/download/";
+				if (!_fileSystem.Directory.Exists(directory)) _fileSystem.Directory.CreateDirectory(directory);
+
+				var filePath = $"{directory}{filename}";
+				await _fileSystem.File.WriteAllBytesAsync(filePath, bytes);
+				bitmap = new Bitmap(filePath);
+			}
+
+			return bitmap;
 		}
 	}
 }
