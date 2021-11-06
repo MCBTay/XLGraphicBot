f662999d2727529e17997ee1a93688d57f809342
Bryan Taylor <mcbtay@gmail.com>
2021-11-05 20:43:30 -0400

    formatting changes

--- XLGraphicBot.UnitTest/Core/GraphicModuleExtensions.cs
+++ XLGraphicBot.UnitTest/Core/GraphicModuleExtensions.cs
@@ -1,81 +1,81 @@
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Discord;
 using Discord.Commands;
 using Moq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
-namespace XLGraphicBot.UnitTest.Core 
+namespace XLGraphicBot.UnitTest.Core
 {
-  [ExcludeFromCodeCoverage]
-  public static class GraphicModuleExtensions
-  {
-    public static void SetupCommandContext(
-      this Mock<ICommandContext> mockCommandContext,
-      ISelfUser selfUser,
-      List<List<IMessage>> messages = null)
-    {  
-      mockCommandContext.SetupMessageChannel(messages);
-      mockCommandContext.SetupDiscordClient(selfUser);
-    }
+	[ExcludeFromCodeCoverage]
+	public static class GraphicModuleExtensions
+	{
+		public static void SetupCommandContext(
+			this Mock<ICommandContext> mockCommandContext,
+			ISelfUser selfUser,
+			List<List<IMessage>> messages = null)
+		{
+			mockCommandContext.SetupMessageChannel(messages);
+			mockCommandContext.SetupDiscordClient(selfUser);
+		}
 
-    private static void SetupMessageChannel(
-      this Mock<ICommandContext> mockCommandContext,
-      List<List<IMessage>> messages = null)
-    {
-      var mockMessageChannel = new Mock<IMessageChannel>();
+		private static void SetupMessageChannel(
+			this Mock<ICommandContext> mockCommandContext,
+			List<List<IMessage>> messages = null)
+		{
+			var mockMessageChannel = new Mock<IMessageChannel>();
 
-      mockMessageChannel
-				.Setup(x => x.GetMessagesAsync(
-					It.IsAny<int>(),
-					It.IsAny<CacheMode>(),
-					It.IsAny<RequestOptions>()))
-				.Returns((messages ?? new List<List<IMessage>>()).ToAsyncEnumerable());
+			mockMessageChannel
+					  .Setup(x => x.GetMessagesAsync(
+						  It.IsAny<int>(),
+						  It.IsAny<CacheMode>(),
+						  It.IsAny<RequestOptions>()))
+					  .Returns((messages ?? new List<List<IMessage>>()).ToAsyncEnumerable());
 
-      mockCommandContext
-				.SetupGet(x => x.Channel)
-				.Returns(mockMessageChannel.Object);
-    }
+			mockCommandContext
+					  .SetupGet(x => x.Channel)
+					  .Returns(mockMessageChannel.Object);
+		}
 
-    private static void SetupDiscordClient(
-      this Mock<ICommandContext> mockCommandContext,
-      ISelfUser selfUser)
-    {
-      var mockDiscordClient = new Mock<IDiscordClient>();
-      
-      mockDiscordClient
-				.SetupGet(x => x.CurrentUser)
-				.Returns(selfUser);
+		private static void SetupDiscordClient(
+			this Mock<ICommandContext> mockCommandContext,
+			ISelfUser selfUser)
+		{
+			var mockDiscordClient = new Mock<IDiscordClient>();
 
-      mockCommandContext
-        .SetupGet(x => x.Client)
-        .Returns(mockDiscordClient.Object);
-    }
-    
-    public static void SetupAttachments(
-      this List<IAttachment> attachments, 
-      int width = 100, 
-      int height = 100,
-      string url = "http://www.google.com",
-      string filename = "image.png") 
-    {
-      foreach (var attachment in attachments)
+			mockDiscordClient
+					  .SetupGet(x => x.CurrentUser)
+					  .Returns(selfUser);
+
+			mockCommandContext
+			  .SetupGet(x => x.Client)
+			  .Returns(mockDiscordClient.Object);
+		}
+
+		public static void SetupAttachments(
+			this List<IAttachment> attachments,
+			int width = 100,
+			int height = 100,
+			string url = "http://www.google.com",
+			string filename = "image.png")
+		{
+			foreach (var attachment in attachments)
 			{
-        var mockAttachment = Mock.Get(attachment);
-				
-        mockAttachment.SetupGet(x => x.Width).Returns(width);
-        mockAttachment.SetupGet(x => x.Height).Returns(height);
+				var mockAttachment = Mock.Get(attachment);
+
+				mockAttachment.SetupGet(x => x.Width).Returns(width);
+				mockAttachment.SetupGet(x => x.Height).Returns(height);
 				mockAttachment.SetupGet(x => x.Url).Returns(url);
 				mockAttachment.SetupGet(x => x.Filename).Returns(filename);
 			}
-    }
+		}
 
-    public static void SetupMessages(
-      this List<List<IMessage>> messages,
-      ulong id,
-      List<IAttachment> attachments = null)
-    {
-      foreach (var messageList in messages)
+		public static void SetupMessages(
+			this List<List<IMessage>> messages,
+			ulong id,
+			List<IAttachment> attachments = null)
+		{
+			foreach (var messageList in messages)
 			{
 				foreach (var message in messageList)
 				{
@@ -88,6 +88,6 @@ namespace XLGraphicBot.UnitTest.Core
 						.Returns(attachments ?? new List<IAttachment>());
 				}
 			}
-    }
-  }
+		}
+	}
 }
