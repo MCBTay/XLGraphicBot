17d87d55073f551aebbdc68d125153fc3f0f3016
Bryan Taylor <mcbtay@gmail.com>
2021-11-06 12:32:56 -0400

    moving help service stuff into the help module, going through discordservice

---
+++ XLGraphicBot.UnitTest/modules/HelpModuleUnitTest.cs
@@ -0,0 +1,118 @@
+﻿using AutoFixture.Xunit2;
+using Discord.Commands;
+using Moq;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using XLGraphicBot.Localization;
+using XLGraphicBot.modules;
+using XLGraphicBot.services;
+using XLGraphicBot.UnitTest.Core;
+using Xunit;
+
+namespace XLGraphicBot.UnitTest.modules
+{
+	[ExcludeFromCodeCoverage]
+	public class HelpModuleUnitTest
+	{
+		[Theory, AutoMoqData]
+		public async Task GetGeneralHelpTextTest(
+			[Frozen] Mock<IDiscordService> mockDiscordService,
+			HelpModule.GeneralHelpModule sut)
+		{
+			await sut.GeneralHelpAsync();
+
+			mockDiscordService
+				.Verify(x => x.SendMessageAsync(
+					It.IsAny<ICommandContext>(),
+					It.Is<string>(y =>
+						y.Contains(Strings.HelpHeader) &&
+						y.Contains(Strings.General_Description) &&
+						y.Contains(Strings.General_Deck) &&
+						y.Contains(Strings.General_Tops) &&
+						y.Contains(Strings.General_Colors)),
+					false,
+					null,
+					null,
+					null,
+					null));
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetDeckHelpText(
+			[Frozen] Mock<IDiscordService> mockDiscordService,
+			HelpModule.GeneralHelpModule sut)
+		{
+			await sut.DeckHelpAsync();
+
+			mockDiscordService
+				.Verify(x => x.SendMessageAsync(
+					It.IsAny<ICommandContext>(),
+					It.Is<string>(y =>
+						y.Contains(Strings.HelpHeader) &&
+						y.Contains(Strings.Deck_Description) &&
+						y.Contains(Strings.Deck_Parameters) &&
+						y.Contains(Strings.Deck_IncludeWear) &&
+						y.Contains(Strings.Deck_MaintainAspectRatio)),
+					false,
+					null,
+					null,
+					null,
+					null));
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetTopsHelpTextTest(
+			[Frozen] Mock<IDiscordService> mockDiscordService,
+			HelpModule.GeneralHelpModule.TopsHelpModule sut)
+		{
+			await sut.TopsHelpAsync();
+
+			mockDiscordService
+				.Verify(x => x.SendMessageAsync(
+					It.IsAny<ICommandContext>(),
+					It.Is<string>(y =>
+						y.Contains(Strings.HelpHeader) &&
+						y.Contains(Strings.Tops_Description) &&
+						y.Contains(Strings.Tops_AlphaMShirt) &&
+						y.Contains(Strings.Tops_BWHoodie) &&
+						y.Contains(Strings.Tops_ESShirt) &&
+						y.Contains(Strings.Tops_MHoodie) &&
+						y.Contains(Strings.Tops_MShirt) &&
+						y.Contains(Strings.Tops_MSweater) &&
+						y.Contains(Strings.Tops_TAShirt) &&
+						y.Contains(Strings.Tops_TLSweater) &&
+						y.Contains(Strings.Tops_Parameters) &&
+						y.Contains(Strings.Tops_Color) &&
+						y.Contains(Strings.Tops_KnownColors) &&
+						y.Contains(Strings.Tops_AARRGGBB) &&
+						y.Contains(Strings.Tops_RRGGBB) &&
+						y.Contains(Strings.Tops_MaintainAspectRatio)),
+					false,
+					null,
+					null,
+					null,
+					null));
+		}
+
+		[Theory, AutoMoqData]
+		public async Task GetTopsColorsHelpTextTest(
+			[Frozen] Mock<IDiscordService> mockDiscordService,
+			HelpModule.GeneralHelpModule.TopsHelpModule sut)
+		{
+			await sut.ColorsHelpAsync();
+
+			mockDiscordService
+				.Verify(x => x.SendMessageAsync(
+					It.IsAny<ICommandContext>(),
+					It.Is<string>(y =>
+						y.Contains(Strings.HelpHeader) &&
+						y.Contains(Strings.TheFullListOfColors) &&
+						y.Contains(Strings.KnownColors)),
+					false,
+					null,
+					null,
+					null,
+					null));
+		}
+	}
+}
--- modules/HelpModule.cs
+++ modules/HelpModule.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Discord.Commands;
 using System.Threading.Tasks;
+using XLGraphicBot.Localization;
 using XLGraphicBot.services;
 
 namespace XLGraphicBot.modules
@@ -11,48 +13,87 @@ namespace XLGraphicBot.modules
         [Alias("?", "halp", "rtfm")]
         public class GeneralHelpModule : ModuleBase<SocketCommandContext>
         {
-	        private readonly IHelpService _helpService;
+	        private readonly IDiscordService _discordService;
 
-	        public GeneralHelpModule(IHelpService helpService)
+	        public GeneralHelpModule(IDiscordService discordService)
 	        {
-		        _helpService = helpService;
+		        _discordService = discordService;
 	        }
 
             [Command]
             public async Task GeneralHelpAsync() 
             {
-	            await ReplyAsync(_helpService.GetGeneralHelpText());
+	            var sb = new StringBuilder();
+
+	            sb.AppendLine(Strings.HelpHeader);
+	            sb.AppendLine(Strings.General_Description);
+	            sb.AppendLine(Strings.General_Deck);
+	            sb.AppendLine(Strings.General_Tops);
+	            sb.AppendLine(Strings.General_Colors);
+
+	            await _discordService.SendMessageAsync(Context, sb.ToString());
             }
 
             [Command("deck")]
             public async Task DeckHelpAsync()
             {
-                await ReplyAsync(_helpService.GetDeckHelpText());
-            }
+	            var sb = new StringBuilder();
+
+	            sb.AppendLine(Strings.HelpHeader);
+	            sb.AppendLine(Strings.Deck_Description);
+	            sb.AppendLine(Strings.Deck_Parameters);
+	            sb.AppendLine(Strings.Deck_IncludeWear);
+	            sb.AppendLine(Strings.Deck_MaintainAspectRatio);
+
+				await _discordService.SendMessageAsync(Context, sb.ToString());
+			}
 
             [Group("tops")]
             public class TopsHelpModule : ModuleBase<SocketCommandContext>
             {
-	            private readonly IHelpService _helpService;
+				private readonly IDiscordService _discordService;
 
-	            public TopsHelpModule(IHelpService helpService)
+				public TopsHelpModule(IDiscordService discordService)
 	            {
-		            _helpService = helpService;
-	            }
+					_discordService = discordService;
+				}
 
                 [Command]
 				public async Task TopsHelpAsync()
 				{
-					await ReplyAsync(_helpService.GetTopsHelpText());
+					var sb = new StringBuilder();
+
+					sb.AppendLine(Strings.HelpHeader);
+					sb.AppendLine(Strings.Tops_Description);
+					sb.AppendLine(Strings.Tops_AlphaMShirt);
+					sb.AppendLine(Strings.Tops_BWHoodie);
+					sb.AppendLine(Strings.Tops_ESShirt);
+					sb.AppendLine(Strings.Tops_MHoodie);
+					sb.AppendLine(Strings.Tops_MShirt);
+					sb.AppendLine(Strings.Tops_MSweater);
+					sb.AppendLine(Strings.Tops_TAShirt);
+					sb.AppendLine(Strings.Tops_TLSweater);
+					sb.AppendLine(Strings.Tops_Parameters);
+					sb.AppendLine(Strings.Tops_Color);
+					sb.AppendLine(Strings.Tops_KnownColors);
+					sb.AppendLine(Strings.Tops_AARRGGBB);
+					sb.AppendLine(Strings.Tops_RRGGBB);
+					sb.AppendLine(Strings.Tops_MaintainAspectRatio);
+
+					await _discordService.SendMessageAsync(Context, sb.ToString());
 				}
 
                 [Command("colors")]
                 public async Task ColorsHelpAsync()
                 {
-                    
+	                var sb = new StringBuilder();
 
-                    await ReplyAsync(_helpService.GetTopsColorsHelpText());
-                }
+	                sb.AppendLine(Strings.HelpHeader);
+	                sb.AppendLine(Strings.TheFullListOfColors);
+	                sb.AppendLine(Strings.KnownColors);
+
+					await _discordService.SendMessageAsync(Context, sb.ToString());
+				}
             }
         }
     }
