913d0789261eac141a5f61399a23c00fe810e4a8
Bryan Taylor <mcbtay@gmail.com>
2021-11-05 20:16:17 -0400

    unit test updates and new ones for BaseTopGraphicModule

--- XLGraphicBot.UnitTest/modules/tops/BaseTopGraphicModuleUnitTest.cs
+++ XLGraphicBot.UnitTest/modules/tops/BaseTopGraphicModuleUnitTest.cs
@@ -4,56 +4,53 @@ using Moq;
 using System.Drawing;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
-using XLGraphicBot.modules;
+using XLGraphicBot.modules.tops;
 using XLGraphicBot.services;
 using XLGraphicBot.UnitTest.Core;
 using Xunit;
 
-namespace XLGraphicBot.UnitTest.modules
+namespace XLGraphicBot.UnitTest.modules.tops
 {
-	public class DeckGraphicModuleUnitTest
+	public class BaseTopGraphicModuleUnitTest
 	{
 		[Theory, AutoMoqData]
-		public async Task DeckAsync_NullImage(
+		public async Task GenerateGraphicAsync_NullImage(
 			[Frozen] Mock<IBitmapService> mockBitmapService,
 			[Frozen] Mock<IDiscordService> mockDiscordService,
-			DeckGraphicModule sut)
+			BaseTopGraphicModule sut)
 		{
 			mockDiscordService
 				.Setup(x => x.GetMostRecentImage(It.IsAny<ICommandContext>()))
 				.ReturnsAsync(((Bitmap)null, "image.jpg"));
 
-			await sut.DeckAsync(null);
+			await sut.GenerateGraphicAsync("mshirt", new Rectangle(0, 0, 100, 100), null);
 
 			mockBitmapService.Verify(x => x.ApplyGraphicToTemplate(It.IsAny<Bitmap>(), It.IsAny<Bitmap>(), It.IsAny<Rectangle>(), It.IsAny<bool>(), It.IsAny<string>()), Times.Never());
 			mockDiscordService.Verify(x => x.SendFileAsync(It.IsAny<ICommandContext>(), It.IsAny<Bitmap>(), It.IsAny<string>()), Times.Never());
 		}
 
 		[Theory, AutoMoqData]
-		public async Task DeckAsync_EmptyFilename(
+		public async Task GenerateGraphicAsync_EmptyFilename(
 			[Frozen] Mock<IBitmapService> mockBitmapService,
 			[Frozen] Mock<IDiscordService> mockDiscordService,
-			DeckGraphicModule sut)
+			BaseTopGraphicModule sut)
 		{
 			mockDiscordService
 				.Setup(x => x.GetMostRecentImage(It.IsAny<ICommandContext>()))
 				.ReturnsAsync((new Bitmap(100, 100), string.Empty));
 
-			await sut.DeckAsync(null);
+			await sut.GenerateGraphicAsync("mshirt", new Rectangle(0, 0, 100, 100), null);
 
 			mockBitmapService.Verify(x => x.ApplyGraphicToTemplate(It.IsAny<Bitmap>(), It.IsAny<Bitmap>(), It.IsAny<Rectangle>(), It.IsAny<bool>(), It.IsAny<string>()), Times.Never());
 			mockDiscordService.Verify(x => x.SendFileAsync(It.IsAny<ICommandContext>(), It.IsAny<Bitmap>(), It.IsAny<string>()), Times.Never());
 		}
 
-		[Theory]
-		[InlineAutoMoqData(false)]
-		[InlineAutoMoqData(true)]
-		public async Task DeckAsync_ValidBitmap_ValidFilename(
-			bool includeWear,
+		[Theory, AutoMoqData]
+		public async Task GenerateGraphicAsync_ValidBitmap_ValidFilename(
 			[Frozen] Mock<IBitmapService> mockBitmapService,
 			[Frozen] Mock<IFileSystem> mockFileSystem,
 			[Frozen] Mock<IDiscordService> mockDiscordService,
-			DeckGraphicModule sut)
+			BaseTopGraphicModule sut)
 		{
 			Mock.Get(mockFileSystem.Object.File)
 				.Setup(x => x.Exists(It.IsAny<string>()))
@@ -63,19 +60,14 @@ namespace XLGraphicBot.UnitTest.modules
 				.Setup(x => x.GetMostRecentImage(It.IsAny<ICommandContext>()))
 				.ReturnsAsync((new Bitmap(100, 100), "image.png"));
 
-			await sut.DeckAsync(new DeckGraphicModuleArguments { IncludeWear = includeWear });
+			await sut.GenerateGraphicAsync("mshirt", new Rectangle(0, 0, 100, 100), null);
 
-			mockBitmapService.Verify(x => x.ApplyGraphicToTemplate(It.IsAny<Bitmap>(), It.IsAny<Bitmap>(), new Rectangle(0, 694, 2048, 482), It.IsAny<bool>(), It.IsAny<string>()), Times.Once());
-
-			if (includeWear)
-			{
-				mockBitmapService.Verify(x => x.ApplyGraphicToTemplate(It.IsAny<Bitmap>(), It.IsAny<Bitmap>(), new Rectangle(0, 0, 2048, 2048), It.IsAny<bool>(), It.IsAny<string>()), Times.Once());
-			}
+			mockBitmapService.Verify(x => x.ApplyGraphicToTemplate(It.IsAny<Bitmap>(), It.IsAny<Bitmap>(), new Rectangle(0, 0, 100, 100), It.IsAny<bool>(), It.IsAny<string>()), Times.Once());
 
 			mockDiscordService.Verify(x => x.SendFileAsync(It.IsAny<ICommandContext>(), It.IsAny<Bitmap>(), It.IsAny<string>()), Times.Once());
 
 			mockFileSystem.Verify(x => x.File.Delete("./img/download/image.png"), Times.Once());
-			mockFileSystem.Verify(x => x.File.Delete("./img/generated/Deck_image.png.png"), Times.Once());
+			mockFileSystem.Verify(x => x.File.Delete("./img/generated/mshirt_image.png.png"), Times.Once());
 		}
 	}
 }
